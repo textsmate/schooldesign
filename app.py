498ffcd366565e3b67c6d54c77de9151b8a6e201
luowanshun <luowanshun@gmail.com>
2020-04-12 09:53:51 +0800

    2

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, request, jsonify
-from exts import db
-from models import User
+from flask import Flask, request, jsonify, render_template, redirect
+from exts import db, login_manager
+from models import User, Seller, Item
+from flask_login import login_user, current_user
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))+r'\static\uploads\\'
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'lllll'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-
+login_manager.init_app(app)
 @app.route('/')
 def index():
     return 'design for school'
@@ -22,6 +26,67 @@ def register():
         "code":1
     }
     return jsonify(msg)
+
+@app.route('/signin',methods=['POST'])
+def signin():
+    name = request.form['name']
+    password = request.form['password']
+    u = User.query.filter(User.name==name).first()
+    msg = {
+        "code":0
+    }
+    if u:
+        if u.password == password:
+            msg['code'] = 1
+    return jsonify(msg)   
+
+@app.route('/seller/signup',methods=['GET', 'POST'])
+def ssignup():
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        f = request.files['avatar']
+        avatar = f.filename
+        f.save(os.path.join(basedir+avatar))
+        u = Seller(name=name, password=password, avatar=avatar)
+        db.session.add(u)
+        db.session.commit()
+        return jsonify({"code":1})
+        # return redirect('/seller/signin')
+    return render_template('signup.html')
+
+@app.route('/seller/signin',methods=['GET', 'POST'])
+def ssignin():
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        s = Seller.query.filter(Seller.name==name).first()
+        if s.password==password:
+            login_user(s)
+            # return jsonify({"code":1})
+            return redirect('/seller/items/add')
+        # return jsonify({"code":0})
+        # return redirect('/seller/signin')
+    return render_template('signin.html')
+
+@app.route('/seller/items/add', methods=['GET', 'POST'])
+def itemsadd():
+    if request.method == 'POST':
+        name = request.form['name']
+        desc = request.form['desc']
+        image = request.files['image']
+        fname = image.filename
+        image.save(os.path.join(basedir+r'item\\'+fname))
+        i = Item(name=name, desc=desc, image=fname, seller=current_user)
+        db.session.add(i)
+        db.session.commit()
+        return redirect('/seller/items')
+    return render_template('itemsadd.html')
+
+@app.route('/seller/items')
+def items():
+    return 'test'
+
 @app.route('/users')
 def users():
     u = User.query.all()
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from exts import db
+from exts import db,login_manager
+from flask_login import UserMixin
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,3 +9,37 @@ class User(db.Model):
     money = db.Column(db.Integer, default=10000)
     def __repr__(self):
         return f"[{self.name}]"
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
+
+class Seller(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String, nullable=False)
+    avatar = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return f"[{self.name}]"
+@login_manager.user_loader
+def load_user(id):
+    return Seller.query.get(id)
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    image = db.Column(db.String(30), nullable=False)
+    desc = db.Column(db.Text, nullable=False)
+    seller_id = db.Column(db.Integer, db.ForeignKey('seller.id'), nullable=False)
+    seller = db.relationship('Seller', backref=db.backref('items'))
+
+    def __repr__(self):
+        return f"[{self.name}]"
+
+class Shoplist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    seller_id = db.Column(db.Integer, db.ForeignKey('seller.id'),nullable=False)
+    seller = db.relationship('Seller', backref=db.backref('shoplists'))
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    item = db.relationship('Item', backref=db.backref('shoplists'))
+    create_time = db.Column(db.DateTime, default=datetime.now)
